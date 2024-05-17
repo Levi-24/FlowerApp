71b7c4e7688eb7cf2778d265013aa0cd37c01444
Levi-24 <leventemajor36@gmail.com>
2024-05-17 11:18:55 +0200

    Add project files.

---
+++ Flower.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerApp
+{
+    internal class Flower
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Color { get; set; }
+        public string Image { get; set; }
+        public string ImageType { get; set; }
+
+        public Flower(string readLine)
+        {
+            var data = readLine.Split(";");
+            Name = data[0];
+            Price = int.Parse(data[1]);
+            Color = data[2];
+
+            var image = data[3].Split(".");
+            Image = image[0];
+            ImageType = image[1];
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FlowerApp
+{
+    public partial class MainWindow : Window
+    {
+        List<Flower> flowers = new List<Flower>();
+        List<Flower> copy = new List<Flower>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (var sr = new StreamReader("../../../src/flowers.txt"))
+            {
+                _ = sr.ReadLine();
+                while (!sr.EndOfStream) flowers.Add(new Flower(sr.ReadLine()));
+            }
+
+            flowers = flowers.OrderBy(x => x.Name).ToList();
+            foreach (var item in flowers) lbxFlower.Items.Add(item.Name);
+            lbxFlower.SelectedIndex = 0;
+        }
+
+        private void lbxFlower_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lbxFlower.SelectedIndex != -1)
+            {
+                int index = lbxFlower.SelectedIndex;
+                var select = flowers[index];
+                txbName.Text = select.Name;
+                txbPrice.Text = select.Price.ToString();
+                txbColor.Text = select.Color;
+
+                string source = "../../../src/IMAGES/" + select.Image + '.' + select.ImageType;
+                image.Source = new ImageSourceConverter().ConvertFromString(source) as ImageSource;
+            }
+            else
+            {
+                txbName.Text = "";
+                txbPrice.Text = "";
+                txbColor.Text = "";
+            }
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            if (lbxFlower.Items.Count > 0)
+            {
+                flowers.RemoveAt(lbxFlower.SelectedIndex);
+
+                lbxFlower.Items.Clear();
+                foreach (var item in flowers) lbxFlower.Items.Add(item.Name);
+
+                lbxFlower.SelectedIndex = 0;
+            }
+            if (lbxFlower.Items.Count == 0)
+            {
+                btnDel.IsEnabled = false;
+                btnCopy.IsEnabled = false;
+            }
+        }
+
+        private void Copy(object sender, RoutedEventArgs e)
+        {
+            copy.Add(flowers[lbxFlower.SelectedIndex]);
+
+            lbxCopy.Items.Clear();
+            foreach (var item in copy)
+            {
+                lbxCopy.Items.Add(item.Name);
+            }
+        }
+    }
+}
